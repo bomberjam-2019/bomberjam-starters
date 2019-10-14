75b3cdb87a157afb5a276bf8bcf0607487c059e4
Anthony Simmon <simmon.anthony@gmail.com>
2019-10-14 17:40:37 -0400

    WIP

---
+++ csharp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using Bomberjam.Client;
+using Bomberjam.Client.Game;
+
+namespace Bomberjam.Bot
+{
+    public class Program
+    {
+        private static readonly GameAction[] AllGameActions = Enum.GetValues(typeof(GameAction)).Cast<GameAction>().ToArray();
+        
+        private static readonly Random Rng = new Random(42);
+        
+        public static void Main()
+        {
+            BomberjamRunner.Run(new BomberjamOptions
+            {
+                Mode = GameMode.Training,
+                BotFunc = GenerateRandomAction
+            });
+        }
+        
+        private static GameAction GenerateRandomAction(GameState state)
+        {
+            return AllGameActions[Rng.Next(AllGameActions.Length)];
+        }
+    }
+}
