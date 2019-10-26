2af2c61e3b260f8f0357efc1150b6ac2d7795ab9
Anthony Simmon <simmon.anthony@gmail.com>
2019-10-25 23:12:21 -0400

    Added F# starter project

---
+++ fsharp/Program.fs
@@ -0,0 +1,22 @@
+﻿open Bomberjam.Client
+open Bomberjam.Client.Game
+open System
+
+let rng = Random()
+
+let allGameActions =
+    Enum.GetValues typeof<GameAction>
+    |> Seq.cast<GameAction>
+    |> Seq.toArray
+
+let generateRandomAction (state: GameState) (myPlayerId: string) =
+    let idx = rng.Next allGameActions.Length
+    allGameActions.[idx]
+
+let generateRandomActionFunc = Func<GameState, string, GameAction>(generateRandomAction)
+
+[<EntryPoint>]
+let main _ =
+    let options = BomberjamOptions(Mode = GameMode.Training, BotFunc = generateRandomActionFunc)
+    BomberjamRunner.Run options
+    0
