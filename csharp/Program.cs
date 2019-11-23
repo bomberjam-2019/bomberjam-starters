eeab1894ab57f902a3a090319bc5ec900f3cf28d
Anthony Simmon <simmon.anthony@gmail.com>
2019-11-23 14:39:01 -0500

    Updated c# client with latest nuget package

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -1,28 +1,72 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Bomberjam.Client;
-using Bomberjam.Client.Game;
 
 namespace Bomberjam.Bot
 {
     public class Program
     {
-        private static readonly GameAction[] AllGameActions = Enum.GetValues(typeof(GameAction)).Cast<GameAction>().ToArray();
-        
         private static readonly Random Rng = new Random(42);
-        
-        public static void Main()
+
+        private static readonly GameAction[] AllActions =
         {
-            BomberjamRunner.Run(new BomberjamOptions
-            {
-                Mode = GameMode.Training,
-                BotFunc = GenerateRandomAction
-            });
-        }
+            GameAction.Stay,
+            GameAction.Left,
+            GameAction.Right,
+            GameAction.Up,
+            GameAction.Down,
+            GameAction.Bomb
+        };
         
+        public static async Task Main()
+        {
+            ParseGamelogExemple("/path/to/some.gamelog");
+            
+            await SimulateExemple();
+            
+            await PlayInBrowserExemple();
+        }
+
+        private static void ParseGamelogExemple(string path)
+        {
+            var gamelog = new Gamelog(path);
+
+            foreach (var step in gamelog)
+            {
+                Console.WriteLine(step.State.Tiles);
+            }
+        }
+
+        private static async Task SimulateExemple()
+        {
+            var simulation = await BomberjamRunner.StartSimulation();
+            
+            while (!simulation.IsFinished)
+            {
+                Console.WriteLine(simulation.CurrentState.Tiles);
+                
+                var playerActions = GenerateRandomActionForAllPlayers(simulation.CurrentState);
+                simulation = await simulation.GetNext(playerActions);
+            }
+        }
+
+        private static IDictionary<string, GameAction> GenerateRandomActionForAllPlayers(GameState state)
+        {
+            return state.Players.ToDictionary(
+                p => p.Key,
+                p => GenerateRandomAction(state, p.Key));
+        }
+
         private static GameAction GenerateRandomAction(GameState state, string myPlayerId)
         {
-            return AllGameActions[Rng.Next(AllGameActions.Length)];
+            return AllActions[Rng.Next(AllActions.Length)];
+        }
+
+        private static Task PlayInBrowserExemple()
+        {
+            return BomberjamRunner.PlayInBrowser(new BomberjamOptions(GenerateRandomAction));
         }
     }
 }
